217ba2c6a16add3ce516365db8aa7c6fc4b00357
Irwaresly <silviairware372@fgmail.com>
2023-10-11 13:55:28 -0700

    Number of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,7 +2,7 @@
 
 
 def number_keys(a_dictionary):
-
-    #A function that returns the number of keys in a dictionary.
-    
+    """
+    A function that returns the number of keys in a dictionary.
+    """
     return len(a_dictionary)
