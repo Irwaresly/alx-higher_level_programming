3a249b8385f996539ebde702a4b0eb04284ac24a
Irwaresly <silviairware372@fgmail.com>
2023-11-11 03:22:51 -0800

    Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-'''Contains a print_square function for a TDD project.
-'''
+"""
+This is the "3-say_my-name" module.
+The 3-say_my_name  module supplies one function, say_my_name.
+"""
 
 
-def print_square(size):
-    '''Prints a square with the character '#'.
-
-    Args:
-        size (int): The size length of the square.
-    '''
-    if not isinstance(size, int):
-        raise TypeError('size must be an integer')
-    elif size < 0:
-        raise ValueError('size must be >= 0')
-    else:
-        for i in range(size):
-            print('{}'.format('#' * size))
+def say_my_name(first_name, last_name=""):
+    """Prints "My name is" followed by the first name and optional last name"""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
