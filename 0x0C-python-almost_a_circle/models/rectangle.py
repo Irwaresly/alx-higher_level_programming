7a6ff4848035544e4721fadfefe945fcb025ef3e
Irwaresly <silviairware372@gmail.com>
2023-11-13 08:48:52 -0800

    commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,43 +1,170 @@
 #!/usr/bin/python3
+
+
+
 from models.base import Base
 
+
+
 class Rectangle(Base):
+
+
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)
+
         self.width = width
+
         self.height = height
+
         self.x = x
+
         self.y = y
 
+        super().__init__(id)
+
+
     @property
+
     def width(self):
+
         return self.__width
 
+
     @width.setter
+
     def width(self, value):
+
+        self.setter_validation("width", value)
+
         self.__width = value
 
+
     @property
+
     def height(self):
+
         return self.__height
 
+
     @height.setter
+
     def height(self, value):
+
+        self.setter_validation("height", value)
+
         self.__height = value
 
+
     @property
+
     def x(self):
+
         return self.__x
 
+
     @x.setter
+
     def x(self, value):
+
+        self.setter_validation("x", value)
+
         self.__x = value
 
+
     @property
+
     def y(self):
+
         return self.__y
 
+
     @y.setter
+
     def y(self, value):
+
+        self.setter_validation("y", value)
+
         self.__y = value
 
+
+    def area(self):
+
+        return (self.height * self.width)
+
+
+    def display(self):
+
+        rectangle = ""
+
+        print("\n" * self.y, end="")
+
+        for i in range(self.height):
+
+            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
+
+        print(rectangle, end="")
+
+
+    def update(self, *args, **kwargs):
+
+        if len(args) == 0:
+
+            for key, val in kwargs.items():
+
+                self.__setattr__(key, val)
+
+            return
+
+        try:
+
+            self.id = args[0]
+
+            self.width = args[1]
+
+            self.height = args[2]
+
+            self.x = args[3]
+
+            self.y = args[4]
+
+        except IndexError:
+
+            pass
+
+
+    def to_dictionary(self):
+
+        return {'x': getattr(self, "x"),
+
+                'y': getattr(self, "y"),
+
+                'id': getattr(self, "id"),
+
+                'height': getattr(self, "height"),
+
+                'width': getattr(self, "width")}
+
+
+    @staticmethod
+
+    def setter_validation(attribute, value):
+
+        if type(value) != int:
+
+            raise TypeError("{} must be an integer".format(attribute))
+
+        if attribute == "x" or attribute == "y":
+
+            if value < 0:
+
+                raise ValueError("{} must be >= 0".format(attribute))
+
+        elif value <= 0:
+
+            raise ValueError("{} must be > 0".format(attribute))
+
+
+    def __str__(self):
+
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
+
+                                                       self.width, self.height)
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+
+
+from models.rectangle import Rectangle
+
+
+
+class Square(Rectangle):
+
+    def __init__(self, size, x=0, y=0, id=None):
+
+        super().__init__(size, size, x, y, id)
+
+
+    @property
+
+    def size(self):
+
+        return self.width
+
+
+    @size.setter
+
+    def size(self, value):
+
+        self.width = value
+
+        self.height = value
+
+
+    def __str__(self):
+
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+
+                                                         self.x,
+
+                                                         self.y,
+
+                                                         self.width)
+
+
+    def update(self, *args, **kwargs):
+
+        if len(args):
+
+            for i, arg in enumerate(args):
+
+                if i == 0:
+
+                    self.id = arg
+
+                elif i == 1:
+
+                    self.size = arg
+
+                elif i == 2:
+
+                    self.x = arg
+
+                elif i == 3:
+
+                    self.y = arg
+
+        else:
+
+            for key, value in kwargs.items():
+
+                if hasattr(self, key) is True:
+
+                    setattr(self, key, value)
+
+
+    def to_dictionary(self):
+
+        return {
+
+            "id": self.id,
+
+            "size": self.size,
+
+            "x": self.x,
+
+            "y": self.y
+
+        }
+
+
