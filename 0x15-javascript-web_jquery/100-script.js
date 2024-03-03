f3bea5ca69d83eaa905cb003c01e747c8a32a203
Irwaresly <silviairware372@gmail.com>
2024-03-03 14:32:01 -0800

    js web query

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // The DOMContentLoaded event ensures that the document has fully loaded
+  // Select the <header> element and update its text color to red
+  const headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+});
+
